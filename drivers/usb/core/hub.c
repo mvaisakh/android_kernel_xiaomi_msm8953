748eba26578c204cf858add99ffba62dab4f8b6f
hehao <hehao@xiaomi.com>
2019-06-06 15:56:12 +0800

    Kernel: Xiaomi kernel changes for Redmi S2
     The Patch based on QualComm release TAG:LA.UM.7.6.r1-03900-89xx.0
     The kernel config file used is ysl-perf_defconfig
    
    Kernel: Upgraded to tag: "LA.UM.8.6.r1-02900-89xx.0"
    
    Change-Id: Icf3201bd434bf1d6324fb39130efef24bea4fde2
    Signed-off-by: hehao <hehao@xiaomi.com>
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4209,6 +4209,7 @@ static int hub_set_address(struct usb_device *udev, int devnum)
  * device says it supports the new USB 2.0 Link PM errata by setting the BESL
  * support bit in the BOS descriptor.
  */
+ /* //HMI_L6252_A01-223 delete by wangyibo at 20190102 start
 static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 {
 	struct usb_hub *hub = usb_hub_to_struct_hub(udev->parent);
@@ -4226,6 +4227,7 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_set_usb2_hardware_lpm(udev, 1);
 	}
 }
+*/
 
 static int hub_enable_device(struct usb_device *udev)
 {
@@ -4547,7 +4549,7 @@ hub_port_init (struct usb_hub *hub, struct usb_device *udev, int port1,
 	/* notify HCD that we have a device connected and addressed */
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
-	hub_set_initial_usb2_lpm_policy(udev);
+
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
--- fs/ext4/xattr.c
+++ fs/ext4/xattr.c
@@ -1044,9 +1044,11 @@ int ext4_xattr_ibody_inline_set(handle_t *handle, struct inode *inode,
 
 	if (EXT4_I(inode)->i_extra_isize == 0)
 		return -ENOSPC;
+
 	error = ext4_xattr_set_entry(i, s, inode);
 	if (error)
 		return error;
+
 	header = IHDR(inode, ext4_raw_inode(&is->iloc));
 	if (!IS_LAST_ENTRY(s->first)) {
 		header->h_magic = cpu_to_le32(EXT4_XATTR_MAGIC);
